5ae6c0bbfc60abaad2f584b51e47ace419f3004d
Douglas Santos <douglas_motasts@outlook.com>
2021-06-19 16:23:23 -0300

    FIX | Ajustes para refresh token da sessao

---
+++ src/modules/users/infra/typeorm/entities/UserTokens.ts
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from "uuid";
+
+import User from "./User";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+
+
+@Entity("users_tokens")
+class UserTokens {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  refresh_token: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @Column()
+  expires_date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { UserTokens };
